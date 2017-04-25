c0833af807de125db0f72bf4fd22a3531d546d64
Calvin Cushman and Hannah Kinne <hannah@>
2017-04-25 13:53:25 -0700

    create flashcards in html, add css styling and write js toggle function

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  $(".flashcard").click(function() {
+    $(this).children().toggle();
+  });
+});
+
+
+
+  // $(".op").click(function(){
+  //   $(".op *").toggle();
+
+  // $(".op h2").toggle();
+//      $(".op p").toggle();
+// })
